6550ca8b877f10e14fd5877e53357c7c62967347
cliff <cliff@cliffs-MacBook-Pro.local>
2023-02-15 22:35:20 +0300

    adding client request to POST to the messages_action endpoint

--- src/main/java/itech/module/RapidproClient.java
+++ src/main/java/itech/module/RapidproClient.java
@@ -7,6 +7,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import org.json.JSONObject;
 import org.json.JSONArray;
@@ -39,31 +40,35 @@ public class RapidproClient {
          HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String responseBody = response.body();
             //  System.out.println("httpGetRequest: " + responseBody);
-          xxx(responseBody);
+            processJson(responseBody);
     }
-     private static void xxx(String xxxx){
-           JSONObject jsonObject = new JSONObject(xxxx);
+     private static void processJson(String bodyResposne)throws URISyntaxException, IOException, InterruptedException{
+           JSONObject jsonObject = new JSONObject(bodyResposne);
           JSONArray resultsArray = jsonObject.getJSONArray("results");
-        //   int limit = Math.min(resultsArray.length(), 100);
           for (int i = 0; i < 100 && i < resultsArray.length(); i++) {
             JSONObject message = resultsArray.getJSONObject(i);
             int message_id = message.getInt("id");
-            // System.out.println("Msg_id: " + message_id);
             int[] arr = new int[message_id];
+
             JSONArray jsonArray = new JSONArray(arr);
-             JSONObject jsonObject2 = new JSONObject();
+            JSONObject jsonObject2 = new JSONObject();
             jsonObject.put("numbers", jsonArray);
             String jsonString = jsonObject2.toString();
+
             System.out.println(jsonString);
-            // HttpRequest request2 = HttpRequest.newBuilder()
-            // .version(HttpClient.Version.HTTP_2)
-            //    .uri(new URI("https://textit.com/api/v2/message_actions.json"))
-            //    .headers("Authorization", "Token f5e24f098f922f7fa7675b471d569ab0c74c9271")
-            //    .POST(HttpRequest.BodyPublishers.ofString("{\"messages\":\"jsonArray\",\"action\":\"delete\"}", StandardCharsets.UTF_8))
-            //    .build();
-            //    HttpResponse<String> response2 = client.send(request2, BodyHandlers.ofString());
-            //    String responseBody2 = response2.body();
-            //      System.out.println("httpGetRequest: " + responseBody2);,
+
+            HttpClient client = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(20))
+            .build();
+            HttpRequest request2 = HttpRequest.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+               .uri(new URI("https://textit.com/api/v2/message_actions.json"))
+               .headers("Authorization", "Token f5e24f098f922f7fa7675b471d569ab0c74c9271")
+               .POST(HttpRequest.BodyPublishers.ofString("{\"messages\":\"jsonArray\",\"action\":\"delete\"}", StandardCharsets.UTF_8))
+               .build();
+               HttpResponse<String> response2 = client.send(request2, BodyHandlers.ofString());
+               String responseBody2 = response2.body();
+                 System.out.println("httpGetRequest: " + responseBody2);
           }
         }
 }
