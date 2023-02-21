dc76c49a0ed0ebf9493965a5d2712a0c69116a2b
cliff <cliff@cliffs-MacBook-Pro.local>
2023-02-21 21:46:21 +0300

    adding scheduler job to the program

--- src/main/java/itech/module/RapidproClient.java
+++ src/main/java/itech/module/RapidproClient.java
@@ -9,24 +9,65 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.json.JSONObject;
+import org.quartz.Job;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
 import org.json.JSONArray;
 
 public class RapidproClient {
 
-    public static void main( String[] args )
+    public static void main( String[] args ) throws SchedulerException
     {
-        System.gc();
-        try {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .startNow()
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                .withIntervalInSeconds(5)
+                .repeatForever())
+                .build();
+
+        JobDetail job = JobBuilder.newJob(HttpClientJob.class)
+                .withIdentity("job1", "group1")
+                .build();   
+
+        // Schedule the job to run at the specified times             
+        scheduler.scheduleJob(job, trigger);
+
+        // Start the scheduler
+        scheduler.start();   
+    }
+
+    public static class HttpClientJob implements Job {
+        @Override
+        public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+            System.gc();
+           try {
             httpGetRequest();
-        } catch (URISyntaxException e) {
+            } catch (URISyntaxException e) {
             e.printStackTrace();
-        } catch (IOException e) {
+           } catch (IOException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
+           } catch (InterruptedException e) {
             e.printStackTrace();
+          }
+    
         }
     }
+
     public static void httpGetRequest() throws URISyntaxException, IOException, InterruptedException {
         HttpClient client = HttpClient.newBuilder()
         .connectTimeout(Duration.ofSeconds(20))
@@ -39,39 +80,44 @@ public class RapidproClient {
             .build();
          HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String responseBody = response.body();
-            //  System.out.println("httpGetRequest: " + responseBody);
-            processJson(responseBody);
+        processJson(responseBody);
     }
      private static void processJson(String bodyResposne)throws URISyntaxException, IOException, InterruptedException{
-           JSONObject jsonObject = new JSONObject(bodyResposne);
+          List<Integer> idList = new ArrayList<>();
+
+        JSONObject jsonObject = new JSONObject(bodyResposne);
           JSONArray resultsArray = jsonObject.getJSONArray("results");
-          for (int i = 0; i < 100 && i < resultsArray.length(); i++) {
-            JSONObject message = resultsArray.getJSONObject(i);
-            int message_id = message.getInt("id");
-            int[] arr = new int[message_id];
+           for (int i = 0; i < 100 && i < resultsArray.length(); i++) {
+            
+             JSONObject message = resultsArray.getJSONObject(i);
+             if(message.get("direction").equals("in")){
+                idList.add(message.getInt("id"));
+             }}
+            int[] idArray = idList.stream().mapToInt(Integer::intValue).toArray();
 
-            JSONArray jsonArray = new JSONArray(arr);
-            JSONObject jsonObject2 = new JSONObject();
-            jsonObject.put("numbers", jsonArray);
-            String jsonString = jsonObject2.toString();
+         JSONArray jsonArray = new JSONArray(Arrays.toString(idArray));
+         
+        JSONObject jsonObject2 = new JSONObject();
+        jsonObject2.put("messages", jsonArray);
+        jsonObject2.put("action", "restore");
 
-            System.out.println(jsonString);
+         String jsonString = jsonObject2.toString();
 
-            HttpClient client = HttpClient.newBuilder()
-            .connectTimeout(Duration.ofSeconds(20))
-            .build();
-            HttpRequest request2 = HttpRequest.newBuilder()
-            .version(HttpClient.Version.HTTP_2)
-               .uri(new URI("https://textit.com/api/v2/message_actions.json"))
-               .headers("Authorization", "Token f5e24f098f922f7fa7675b471d569ab0c74c9271")
-               .POST(HttpRequest.BodyPublishers.ofString("{\"messages\":\"jsonArray\",\"action\":\"delete\"}", StandardCharsets.UTF_8))
-               .build();
-               HttpResponse<String> response2 = client.send(request2, BodyHandlers.ofString());
-               String responseBody2 = response2.body();
-                 System.out.println("httpGetRequest: " + responseBody2);
-          }
-        }
-}
+        HttpClient client2 = HttpClient.newBuilder()
+        .connectTimeout(Duration.ofSeconds(20))
+        .build();
+        HttpRequest request2 = HttpRequest.newBuilder()
+        .version(HttpClient.Version.HTTP_2)
+           .uri(new URI("https://textit.com/api/v2/message_actions.json"))
+           .headers("Authorization", "Token f5e24f098f922f7fa7675b471d569ab0c74c9271")
+           .header("Content-Type", "application/json")
+           .POST(HttpRequest.BodyPublishers.ofString(jsonString, StandardCharsets.UTF_8))
+           .build();
+           HttpResponse<String> response2 = client2.send(request2, BodyHandlers.ofString());
+            String responseBody2 = response2.body();
+            System.out.println("httpGetRequest: " + responseBody2);
+          }  
+  }
 
 
 
